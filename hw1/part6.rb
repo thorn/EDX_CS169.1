ee34cb341f760a3916cf68e82787dd92707e8204
Thorn731 <notdefined@list.ru>
2013-01-27 22:54:17 +0400

    on a right way

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -1,17 +1,37 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0}
+
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
+
     if @@currencies.has_key?(singular_currency)
-      self * @@currencies[singular_currency]
+      result = self * @@currencies[singular_currency]
+      result.instance_variable_set("@currency", singular_currency)
+      result
     else
       super
     end
   end
+
+  def in(curr)
+    self * @@currencies[@currency]
+  end
+
 end
+p 6.euro.in(:dollar)
+# 10 долларов в евро = 10 * 1 * 1/1.292
+
+# 10 евро в долларах = 10 * 1.292 * 1/1
+
+# 10 рупий в долларах = 10 * 0.019 * 1/1
+
+# 10 рупий в евро = 10 * 0.019 * 1/1.292
 
 class String
-  # YOUR CODE HERE
+  def palindrome?
+    cleared = self.split(/[^a-zA-Z]/i).reject{|s| s.empty? }.map(&:downcase).join('')
+    cleared.reverse == cleared
+  end
 end
 
 module Enumerable
