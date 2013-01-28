35025ff309ca3754fad05efc0da2cda19fb8fa41
Thorn731 <notdefined@list.ru>
2013-01-28 11:18:07 +0400

    little hack for hashes and honestly looping through arrays

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -23,13 +23,19 @@ end
 class String
   def palindrome?
     cleared = self.split(/[^a-zA-Z1-9]/i).reject{|s| s.empty? }.map(&:downcase).join('')
-    p cleared
     cleared.reverse == cleared
   end
 end
 
 module Enumerable
   def palindrome?
-    self.join(' ').palindrome?
+    # self.join(' ').palindrome?
+    return false if self.class == Hash
+    self.all? do |el|
+      i = self.index(el)
+      self[i] == self[(self.length - i - 1)]
+    end
   end
 end
+# hash = {"hello" => "world" }
+# p [1,2,1].palindrome?
